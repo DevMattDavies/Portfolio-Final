5ad7193022f84e185f4c3d9418319b6f8b7149fd
Matt Davies <matt@mattdavies.dev>
2022-08-21 18:14:07 +0100

    Webkit additions for background video

--- components/BackgroundVideo.jsx
+++ components/BackgroundVideo.jsx
@@ -5,13 +5,15 @@ const BackgroundStyle = {
   width: "100%",
   height: "100%",
   // padding: "2rem",
-  position: "fixed",
+  position: "absolute",
   top: "0",
   right: "0",
   // display: "block",
   overflow: "hidden",
   objectFit: "fill",
   zIndex: "-10",
+  maxWidth: "-webkit-fill-available",
+  maxHeight: "-webkit-fill-available",
 };
 
 export default function BackgroundVideo() {
