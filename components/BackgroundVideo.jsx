2072fbe8f6f9e875569b842d0c4dc5b6ca0a32c8
Matt Davies <matt@mattdavies.dev>
2022-08-21 18:59:19 +0100

    Further video fixes

--- components/BackgroundVideo.jsx
+++ components/BackgroundVideo.jsx
@@ -2,17 +2,15 @@ import styled from "styled-components";
 import BackgroundGraphic from "../public/assets/background.mp4";
 
 const backgroundStyle = {
-  minWidth: "99%",
-  minHeight: "99%",
-  maxWidth: "100%",
-  maxHeight: "100%",
-  // width: "auto !important",
-  // height: "auto !important",
-  // height: "auto",
-  // width: "auto",
-  // position: "absolute",
-  // top: "0",
-  // right: "0",
+  minWidth: "100vw",
+  minHeight: "100vh",
+  maxWidth: "101vw",
+  maxHeight: "101vh",
+  width: "auto !important",
+  height: "auto !important",
+  position: "fixed",
+  top: "0",
+  right: "0",
   overflow: "hidden",
   objectFit: "fill",
   zIndex: "-10",
@@ -33,11 +31,11 @@ const BackgroundContainer = styled.div`
 export default function BackgroundVideo() {
   return (
     <>
-      <BackgroundContainer>
-        <video style={backgroundStyle} autoPlay loop muted playsInline>
-          <source src={BackgroundGraphic} type="video/mp4" />
-        </video>
-      </BackgroundContainer>
+      {/* <BackgroundContainer> */}
+      <video style={backgroundStyle} autoPlay loop muted playsInline>
+        <source src={BackgroundGraphic} type="video/mp4" />
+      </video>
+      {/* </BackgroundContainer> */}
     </>
   );
 }
